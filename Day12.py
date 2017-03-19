8a8682c1a206147bac59f6628469f45d77f7e63e
Bryan Pham <Phamtasticc@gmail.com>
2017-03-19 07:27:21 -0700

    Day 12

---
+++ Day12.py
@@ -0,0 +1,39 @@
+class Student(Person):
+    #   Class Constructor
+    #
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+
+    def __init__(self, firstName, lastName, idNumber, scores):
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def calculate(self):
+        total = 0
+
+        for score in self.scores:
+            total += score
+
+        avg = total / len(self.scores)
+
+        if 90 <= avg <= 100:
+            return 'O'
+        if 80 <= avg < 90:
+            return 'E'
+        if 70 <= avg < 80:
+            return 'A'
+        if 55 <= avg < 70:
+            return 'P'
+        if 40 <= avg < 55:
+            return 'D'
+        return 'T'
+
